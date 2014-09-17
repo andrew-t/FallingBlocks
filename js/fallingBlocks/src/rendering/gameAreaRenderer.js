e130e9db1f1b7498024a03fbfa1fbfa530c46da4
Andrew Taylor <andrew.taylor@webappuk.com>
2014-09-17 12:54:47 +0100

    now giles

--- js/fallingBlocks/src/rendering/gameAreaRenderer.js
+++ js/fallingBlocks/src/rendering/gameAreaRenderer.js
@@ -2,16 +2,24 @@ var fallingBlocks = fallingBlocks || {};
 fallingBlocks.rendering = fallingBlocks.game || {};
 
 fallingBlocks.rendering.gameAreaRenderer = function (blockSize, width, height, colours) {
+    var giles;
     return {
         render: function (context, landedBlockLocations, fallingObjectLocations) {
+            // load giles
+            if (!giles) {
+                giles = new Image();
+                giles.src = 'img/giles.png';
+                return;
+            }
+
             // render background
             context.fillStyle = colours.background;
             context.fillRect(0, 0, width, height);
+            //context.drawImage(giles, 0, 0, width, height);
 
             // render landed blocks
             landedBlockLocations.forEach(function (blockLocation) {
-                context.fillStyle = colours.landedBlock;
-                context.fillRect(
+                context.drawImage(giles, 
                     blockLocation.x * blockSize,
                     height - blockSize - (blockLocation.y * blockSize),
                     blockSize,
@@ -20,8 +28,7 @@ fallingBlocks.rendering.gameAreaRenderer = function (blockSize, width, height, c
 
         // render falling object blocks
             fallingObjectLocations.forEach(function (blockLocation) {
-                context.fillStyle = colours.tetrimino;
-                context.fillRect(
+                context.drawImage(giles, 
                     blockLocation.x * blockSize,
                     height - blockSize - (blockLocation.y * blockSize),
                     blockSize,
